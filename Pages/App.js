cf47744b2ad134b34cc4b5ee78596c68d05e737c
yangyang <yangyang@g7.com.cn>
2018-10-19 10:16:12 +0800

    测试canvas画图

--- Pages/App.js
+++ Pages/App.js
@@ -7,16 +7,16 @@
  */
 
 import { Container } from 'flux/utils';
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, ART} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, ART } from 'react-native';
 
 import AppStore from '../src/Stores/AppStore';
 import AppApi from '../src/Api/AppApi';
 
 // import Canvas from 'react-native-canvas';
-import Canvas, {Image as CanvasImage, Path2D} from 'react-native-canvas';
+import Canvas, { Image as CanvasImage, Path2D } from 'react-native-canvas';
 
-const {Surface, Shape, Path,Group} = ART;
+const { Surface, Shape, Path, Group } = ART;
 
 class App extends Component {
     static getStores() {
@@ -44,52 +44,63 @@ class App extends Component {
         const ctx = canvas.getContext('2d');
         ctx.fillStyle = 'purple';
         ctx.fillRect(0, 0, 100, 100);
+        ctx.fillStyle = 'red';
+        ctx.fillText('测试文本', 100, 100, 100);
     }
 
     handleImageRect = (canvas) => {
         const image = new CanvasImage(canvas);
-        canvas.width = 320;
-        canvas.height = 320;
+        canvas.width = 100;
+        canvas.height = 100;
+
+        const context = canvas.getContext('2d');
+        const that = this;
+        image.src = 'https://image.freepik.com/free-vector/unicorn-background-design_1324-79.jpg';
+        image.addEventListener('load', () => {
+            context.drawImage(image, 0, 0, 50, 50);
+            console.log('绘制文字', context);
+            that.test(context);
+        });
+    }
+    test = (context) => {
+        context.fillStyle = 'black';
+        context.fillText('测试文本', 0, 0);
+    }
+
+    handleEmbedHTML = (canvas) => {
+        const image = new CanvasImage(canvas);
+        canvas.width = 100;
+        canvas.height = 100;
 
         const context = canvas.getContext('2d');
 
-        image.src = 'https://image.freepik.com/free-vector/unicorn-background-design_1324-79.jpg';
+        const htmlString = '<b>Hello, World!</b>';
+        const svgString = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+        <foreignObject width="100%" height="100%">
+            <div xmlns="http://www.w3.org/1999/xhtml" style="font-size: 40px; background: lightblue; width: 100vw; height: 100vh;">
+              <span style="background: pink;">
+                ${htmlString}
+              </span>
+            </div>
+        </foreignObject>
+    </svg>
+    `;
+        image.src = `data:image/svg+xml,${encodeURIComponent(svgString)}`;
+
         image.addEventListener('load', () => {
-          context.drawImage(image, 0, 0, 100, 100);
+            context.drawImage(image, 0, 0, 100, 100);
         });
     }
 
     render() {
-        const {list} = this.state;
-        // const pathRect = new Path()
-        //     .moveTo(1,1)
-        //     .lineTo(1,99)
-        //     .lineTo(99,99)
-        //     .lineTo(99,1)
-        //     .close();
-
-        // const pathCircle = new Path()
-        //     .moveTo(50,1)
-        //     .arc(0,99,25)
-        //     .arc(0,-99,25)
-        //     .close();
-
-        // const pathText = new Path()
-        //     .moveTo(40,5)
-        //     .lineTo(40,99);
-
+        const { list } = this.state;
         return (
             <View style={styles.container}>
                 {list.length > 0 && this.showRender()}
                 <Canvas ref={this.handleCanvas} />
                 <Canvas ref={this.handleImageRect} />
-                {/* <Surface width={100} height={100}>
-                    <Group>
-                        <Shape d={pathRect} stroke="#000000" fill="#000000" strokeWidth={1}/>
-                        <Shape d={pathCircle} stroke="#FFFFFF" fill="#FFFFFF" strokeWidth={1}/>
-                        <Text strokeWidth={1} strokeDash={[2,1,2,1]} stroke="#000" font="bold 30px Heiti SC" path={pathText} >Swipe</Text>
-                    </Group>
-                </Surface> */}
+                <Canvas ref={this.handleEmbedHTML} />
             </View>
         );
     }
